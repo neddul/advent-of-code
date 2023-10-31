c456a553632e3c3894d7580615aa2677e893cded
neddul <ludvig.westerholm.7983@student.uu.se>
2023-10-31 14:10:55 +0100

    Day 3 part 1 done

---
+++ 2021/0312/binarydiagnosticp1.py
@@ -0,0 +1,33 @@
+file = open("input.txt")
+readings = file.readlines()
+file.close()
+
+readings = [s.strip() for s in readings]
+
+gamma = ""
+eps = ""
+
+for i in range(len(readings[0])):
+    ones = 0
+    zeroes = 0
+    for stream in readings:
+        if stream[i] == "0":
+            ones += 1
+        else:
+            zeroes +=1
+
+    if ones > zeroes:
+        gamma+= "1"
+        eps+= "0"
+    else:
+        gamma+= "0"
+        eps+= "1"
+
+gamma   = int(gamma, 2)
+eps     = int(eps, 2)
+
+print(gamma)
+print(eps)
+
+power_consumption = gamma*eps
+print(power_consumption)
