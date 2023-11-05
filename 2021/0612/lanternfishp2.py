ddfe2236fbf66257aef6252d4ac4386b67af14f6
neddul <ludvig.westerholm.7983@student.uu.se>
2023-11-05 21:30:40 +0100

    AOC 2021 Day 6 done (ALL BY MYSELF, NO HELP WAS USED OR GOTTEN)

---
+++ 2021/0612/lanternfishp2.py
@@ -0,0 +1,26 @@
+# #Part one
+with open("input.txt",'r') as f:
+    data = f.read()
+
+rows = data.split(',')
+rows = [int(x) for x in rows]
+
+arr = [0]*9
+
+for x in rows:
+    arr[x] +=1
+
+for i in range(256):
+    more = arr[0]
+    arr = arr[1:] + [arr[0]]
+    if more > 0:
+        arr[6] += more
+
+fish = 0
+for x in arr:
+    fish += x
+
+print(fish)
+
+
+
