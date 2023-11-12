0a7f1fc6f542dac99c2c065e2f022c0f6d6f1aee
neddul <ludvig.westerholm.7983@student.uu.se>
2023-11-12 22:18:38 +0100

    AOC 2021 Day 9 done

--- 2021/0912/smokebasinp2.py
+++ 2021/0912/smokebasinp2.py
@@ -9,7 +9,34 @@ def low_point(point, neighbors):
             return False
     return True
 
+def get_neighbors(x, y, board, neighbors):
+    if y-1 >= 0 and y-1 <=len(board)-1 and x >=0 and x <= len(board[0])-1 and (y-1, x) not in neighbors:
+        if int(board[y-1][x]) < 9 :
+            neighbors.append((y-1, x))
+            get_neighbors(x, y-1, board, neighbors)
+        
+    if y+1 >= 0 and y+1 <=len(board)-1 and x >=0 and x <= len(board[0])-1 and (y+1, x) not in neighbors:
+        if int(board[y+1][x]) < 9 :
+            neighbors.append((y+1, x))
+            get_neighbors(x, y+1, board, neighbors)
+        
+    if y >= 0 and y <=len(board)-1 and x-1 >=0 and x-1 <= len(board[0])-1 and (y, x-1) not in neighbors :
+        if int(board[y][x-1]) < 9 :
+            neighbors.append((y, x-1))
+            get_neighbors(x-1, y, board, neighbors)
+        
+    if y >= 0 and y <=len(board)-1 and x+1 >=0 and x+1 <= len(board[0])-1 and (y, x+1) not in neighbors:
+        if int(board[y][x+1]) < 9 :
+            neighbors.append((y, x+1))
+            get_neighbors(x+1, y, board, neighbors)
+
+def basin_size(x, y, board):
+    neighbors = [(y, x)]
+    get_neighbors(x, y, board, neighbors)    
+    return len(neighbors)
+
 risk_sum = 0
+basins = []
 for y in range(len(rows)):
     row = rows[y]
     for x in range(len(row)):
@@ -35,5 +62,10 @@ for y in range(len(rows)):
         
         if islowpoint:
             risk_sum+=int(row[x])+1
+            basins.append(basin_size(x, y, rows))
 
-print(risk_sum)        
+print(risk_sum)
+basin = 1
+for x in list(reversed(sorted(basins)))[:3]:
+    basin*=x
+print(basin)    
