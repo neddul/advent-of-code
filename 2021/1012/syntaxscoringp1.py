b51e881e5f175dfbbd34c530e28f9110aebc39d4
neddul <ludvigwesterholm98@gmail.com>
2023-12-01 08:09:50 +0100

    AOC 2021 Day 10 part 1

--- 2021/1012/syntaxscoringp1.py
+++ 2021/1012/syntaxscoringp1.py
@@ -1,11 +1,6 @@
-with open("input_test.txt",'r') as f:
+with open("input.txt",'r') as f:
     data = f.read()
-
 rows = data.split('\n')
-
-print(rows)
-
-openers = []
 
 closer_points = {
     ')' : 3,
@@ -14,34 +9,31 @@ closer_points = {
     '>' : 25137
 }
 
-closers = set(list(closer_points.keys()))
+corresponding_list = {
+    ')' : '(',
+    ']' : '[',
+    '}' : '{',
+    '>' : '<'
+}
 
-openers = {'(', '[', '{', '<'}
+closers = set(list(closer_points.keys()))
+openers = set(list(corresponding_list.values()))
 
 illegal_closes = 0
-
-opener_list = []
-
-corresponding_list = {
-    '(' : ')',
-    '[' : ']',
-    '{' : '}',
-    '<' : '>'
-}
+stack = []
 
 for row in rows:
     for c in row:
-        print(c)
         if c in openers:
-            opener_list.append(c)
-        elif len(opener_list) == 0:
-            illegal_closes += closer_points[c]
-        elif opener_list[-1] != corresponding_list[opener_list[-1]]:
-            illegal_closes += closer_points[c]
+            stack.append(c)
         else:
-            opener_list.pop()
+            if len(stack) == 0:
+                illegal_closes += closer_points[c]
+                break
+            elif stack[-1] != corresponding_list[c]:
+                illegal_closes += closer_points[c]
+                break
+            else:
+                stack.pop()
 
 print(illegal_closes)
-
-
-
