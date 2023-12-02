d1878ee1b39f9fff53e8df3119ffad92ed8d8c71
neddul <ludvigwesterholm98@gmail.com>
2023-12-02 12:35:29 +0100

    Cleaner, not clean

--- 2023/0212/cubeconundrump2.py
+++ 2023/0212/cubeconundrump2.py
@@ -3,26 +3,18 @@ with open("input.txt",'r') as f:
 rows = data.split('\n')
 
 game_sum = 0
-
 for row in rows:
     game_id = row.split(':')
     items = game_id[1].split(';')
-    info = []
-    for item in items:
-        item = [a.split() for a in item.split(',')]
-        info.append(item)
-
-    bag = {'red' : 0, 'green' : 0, 'blue' : 0}
-    valid_game = True
-    for item_set in info:
-        for item in item_set:
-            if int(item[0]) > bag[item[1]]:
-                bag[item[1]] = int(item[0])
     
-    red = bag['red']
-    blue = bag['blue']
-    green = bag['green']
+    bag = {'red' : 0, 'green' : 0, 'blue' : 0}
+    for item in items:
+        item = [a.split() for a in item.split(',')]        
+        for bricks in item:
+            if int(bricks[0]) > bag[bricks[1]]:
+                bag[bricks[1]] = int(bricks[0])
 
+    red, blue, green = bag['red'], bag['blue'], bag['green']
     game_sum += green*blue*red
 
 print(game_sum)
