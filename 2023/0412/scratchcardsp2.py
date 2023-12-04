ccf5464431a717a0f9da9eccec2617d2e34a9411
neddul <ludvigwesterholm98@gmail.com>
2023-12-04 07:54:14 +0100

    Speed improvement p2

--- 2023/0412/scratchcardsp2.py
+++ 2023/0412/scratchcardsp2.py
@@ -1,7 +1,7 @@
 with open("input.txt",'r') as f:
     data = f.read()
 rows = data.split('\n')
-
+# Shit parsning
 cards = []
 for x in rows:
     cards.append(x.split(' | '))
@@ -12,32 +12,25 @@ your_cards = []
 for x in cards:
     a = x[0].split(': ')
     a = a[1:]
-    a = a[0].split(' ')
+    a = a[0].split()
     winning_cards.append(a)
-    b = x[1].split(' ')
+    b = x[1].split()
 
     your_cards.append(b)
 
-
+# Shit parsning
 winning_cards = [[int(x) for x in a if x.isdigit()] for a in winning_cards]
 your_cards = [[int(x) for x in a if x.isdigit()] for a in your_cards]
 number_of_cards = [1 for x in range(len(winning_cards))]
-    
-list_of_all_cards = []
 
-score = 0
-i= 0
+for w, m, x, i in zip(winning_cards, your_cards, number_of_cards, [x for x in range(len(winning_cards))]):
+    winning_numbers = 0
+    for number in m:
+        if number in w:
+            winning_numbers +=1
 
-for w, m, x in zip(winning_cards, your_cards, number_of_cards):
-    for _ in range(x):     
-        winning_numbers = 0
-        for number in m:
-            if number in w:
-                winning_numbers +=1
-
-        if winning_numbers > 0:
-            for j in range(winning_numbers):
-                number_of_cards[j+i+1] +=1
-    i +=1
+    if winning_numbers > 0:
+        for j in range(winning_numbers):
+            number_of_cards[j+i+1] +=x
     
 print(sum(number_of_cards))
