22d2af91bea4da8cb77965ba6b2d4a4901e03f26
neddul <ludvigwesterholm98@gmail.com>
2023-12-06 08:54:47 +0100

    Improvements

--- 2023/0612/p1.py
+++ 2023/0612/p1.py
@@ -2,16 +2,31 @@ with open("input.txt",'r') as f:
     data = f.read()
 rows = data.split('\n')
 
-def accelerate(time, goal_length):
-    distance_covered = []
-    for i in range(time):
-        distance = i*(time-i)
-        distance_covered.append(distance)
-    
-    distance_covered = [x for x in distance_covered if x > goal_length]
-    return len(distance_covered)
+time = rows[0].split()[1:]
+distance = rows[1].split()[1:]
 
-a, b, c, d = accelerate(44, 208), accelerate(80, 1581), accelerate(65, 1050), accelerate(72, 1102)
-g = a*b*c*d
-print(g) #P1
-print(accelerate(44806572, 208158110501102)) #P2
+def accelerate(time, goal_length):
+    time = time+1
+    for i in range(time):
+        distance = i*(time-i-1)
+        if distance > goal_length:
+            if time % 2 == 0:
+                return time - (i*2)
+            else:
+                return time - ((i)*2)
+
+sum = 1
+for t, d in zip(time, distance):
+    a = accelerate(int(t), int(d))
+    sum *= a
+print(sum) #P1
+
+print(accelerate(int(''.join(time)), int(''.join(distance)))) #P2
+
+# 0 1 2 3 4 5 6 7
+#  \           /
+#   \         /
+# --------------------
+#     \     /
+#      \   /
+#       \ /
