b1b850dd74d12f91627882529557a4827df6f75f
neddul <ludvigwesterholm98@gmail.com>
2023-12-09 20:44:31 +0100

    failed attempt at AOC 2023 day 5 part two

--- 2023/0512/p2.py
+++ 2023/0512/p2.py
@@ -5,101 +5,90 @@ rows = data.split('\n\n')
 seed_info = [int(x) for x in rows[0][7:].split()]
 seed_info = [seed_info[i:i+2] for i in range(0, len(seed_info), 2)]
 
-
-
-# data = set()
-# for s in seed_info:
-#     s0, s1 = s[0], s[1]
-#     data.update(range(s0, s0 + s1))
-    
-
-
-import concurrent.futures
-def process_seed(s):
-    s0, s1 = s[0], s[1]    
-    return set(range(s0, s0 + s1))
-
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=14) as executor:
-    data_sets = list(executor.map(process_seed, seed_info))
-
-result_data = set().union(*data_sets)
-seeds = list(result_data)
-# print(result_data)
-
-# print(data)
-
-
+seed_ranges = []
+for seeds in seed_info:
+    seed_ranges.append((seeds[0], seeds[0]+seeds[1] -1))
+print(seed_ranges)
 
 
 map_info = [[[int(y) for y in x.split()] for x in row.split('\n')[1:]] for row in rows[1:]]
-# ranges = [{},{},{},{},{},{},{}]
+map_range = [{},{},{},{},{},{},{}]
 
-# for info, r in zip(map_info, ranges):
-#     for line in info:
-#         destination_range_start = line[0]
-#         source_range_start = line[1]
-#         range_length = line[2]
-#         r[(source_range_start, source_range_start+range_length -1)] = (destination_range_start, destination_range_start+range_length-1)
-#         # r[source_range_start] = source_range_start + range_length -1
-# print(ranges)
-
-# seeds = []
-# for s in seed_info:
-#     start = s[0]
-#     stop = s[1] - 1
-#     for r in ranges:
-#         for k, v in r.items():
-#             if start >= k and stop <= v: #In the range and will behave the same
-#                 if start not in seeds : seeds.append(start)
-#                 if stop not in seeds : seeds.append(stop)
+for info, r in zip(map_info, map_range):
+    for line in info:
+        d_start_range = line[0]
+        s_start_range = line[1]
+        range_length = line[2]
+        offset = d_start_range - s_start_range
+        r[(s_start_range, s_start_range+range_length -1)] = offset
+#If number in range add offset
+def print_ranges(map_range):
+    for x in map_range:
+        print(x)
+print_ranges(map_range)
 
 
 
+i = 1
+lowest_values = []
+for seed in seed_ranges:
+    list_of_ranges = [seed] #Start with the seed range
+    for map in map_range:
+        list_of_temporary_ranges = []
+        for s in list_of_ranges:
+            seed_start, seed_stop = s[0], s[1]
 
+            outside = True
+            for range, offset in map.items():
+                map_start, map_stop = range[0], range[1]
+                #------------|------|------------
+                #----------|----------|----------
+                if map_start <= seed_start and map_stop >= seed_stop: #Sum the offset on the start and stop of the range
+                    list_of_temporary_ranges.append((seed_start+offset, seed_stop+offset))
+                    outside = False
+
+                #--------|--------------|--------
+                #----------|----------|----------
+                elif seed_start < map_start and seed_stop > map_stop: #Three new ranges are made
+                    list_of_temporary_ranges.append((seed_start, map_start-1)) #Range smaller than start up to start
+                    list_of_temporary_ranges.append((map_start+offset, map_stop+offset)) #Range in the map range
+                    list_of_temporary_ranges.append((map_stop+1, seed_stop)) #Range larger than map stop up to seed stop
+                    outside = False
+
+                #-------|----------|-------------
+                #----------|----------|----------
+                elif map_start > seed_start and seed_stop > map_start and seed_stop < map_stop: # Two new ranges are made
+                    list_of_temporary_ranges.append((seed_start, map_start-1)) #Range smaller than start up to start
+                    list_of_temporary_ranges.append((map_start+offset, seed_stop+offset)) #Range in the map range
+                    outside = False
+
+                #-------------|----------|-------
+                #----------|----------|----------
+                elif map_start < seed_start and seed_start < map_stop and map_stop < seed_stop:
+                    list_of_temporary_ranges.append((seed_start+offset, map_stop+offset)) #Range smaller than start up to start
+                    list_of_temporary_ranges.append((map_stop+1, seed_stop)) #Range smaller than start up to start
+                    outside = False
+
+            if outside:
+                #------------------------|----|--
+                #----------|----------|----------
+                #or
+                #--|----|------------------------
+                #----------|----------|----------
+                list_of_temporary_ranges.append((seed_start, seed_stop)) #Range smaller than start up to start
+
+        print(list_of_temporary_ranges)
+        list_of_ranges = list_of_temporary_ranges
+        
     
-    
-
-list_of_maps = [{},{},{},{},{},{},{}]
-
-for s in seeds:
-    k = s
-    for d, info in zip(list_of_maps, map_info):
-        d[k] = k    
-        for line in info:
-            destination_range_start = line[0]
-            source_range_start = line[1]
-            range_length = line[2]
-            if source_range_start <= k and source_range_start + range_length > k: # Value will be affected
-                diff = abs(destination_range_start-source_range_start)
-                if source_range_start >= destination_range_start:
-                    d[k] -= diff
-                else:            
-                    d[k] += diff
-        k = d[k]
-
-
-lowest_loc = 1000000000000000000000000000000000000000000000
-for seed in seeds:
-    key = seed
-    for my_map in list_of_maps:
-        key = my_map[key]
-    if key < lowest_loc:
-        lowest_loc = key
-
-print(lowest_loc)
-
-
-def print_maps():
-    for s in seeds:
-        info = []
-        k = s
-        info.append(k)
-        for i in range(len(list_of_maps)):
-            v = list_of_maps[i][k]
-            info.append(v)
-            k = v
-        # info = [info[0]] + [info[1]]
-        print(info)
-
-# print_maps()
+    print(list_of_ranges)
+    current_lowest_value = 10**30
+    for range in list_of_ranges:
+        if range[0] < current_lowest_value: #The range will always have the lowest value on the start
+            current_lowest_value = range[0]
+            
+    print(current_lowest_value)
+    lowest_values.append(current_lowest_value)
+print(lowest_values)
+print(min(lowest_values))
+                
