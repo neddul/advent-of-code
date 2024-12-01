f58901f9ad7cc4351f62e4fbf7f214b50b6ea074
neddul <ludvigwesterholm98@gmail.com>
2024-12-01 08:59:41 +0100

    AOC2024 Day 1 done

--- 2024/0112/historianhysteria.py
+++ 2024/0112/historianhysteria.py
@@ -2,20 +2,25 @@ with open("input.txt",'r') as f:
     data = f.read()
 rows = data.split('\n')
 
-print(rows)
-
 leftside = []
 rightside = []
 for row in rows:
     a = row.split()
     leftside.append(int(a[0]))
     rightside.append(int(a[1]))
+    
+
 
 leftside.sort()
 rightside.sort()
 
 my_sum = 0
+my_sum2 = 0
 for l, r in zip(leftside, rightside):
     my_sum += abs(l-r)
+    my_sum2 += l * rightside.count(l) #part 2
+
+
 
 print(my_sum)
+print(my_sum2)
