04125f26f9dfd6814361333a166dc90d47500fa8
neddul <ludvigwesterholm98@gmail.com>
2025-12-01 19:04:36 +0100

    AOC2025 Day 1 done

--- 2025/01/secretentrance.py
+++ 2025/01/secretentrance.py
@@ -4,16 +4,45 @@ rows = data.split('\n')
 
 number = 50
 counter = 0
+counter2 = 0
+
 
 for rotate in rows:
+    nocountzero = True
     direction = rotate[0]
     nudges = int(rotate[1:])
+    
+    last_num = number
+    new_num = nudges 
+    
+    hundreds = int(nudges / 100)
 
-    number = number + nudges if direction == "R" else number - nudges
-    number = number % 100
+    counter2 += hundreds
 
-    if number == 0:
+    offset = nudges % 100
+    number2 = number + nudges if direction == "R" else number - nudges
+
+    number = number + offset if direction == "R" else number - offset
+
+    if number2  % 100 == 0:
         counter += 1
+    
+    if number == 0:
+        counter2 += 1
+    else:
+        if number < 0:
+            number +=100
+            counter2 += 1
+        elif number > 99:
+            number -= 100
+            counter2 += 1
+    
+    if direction == "L" and last_num == 0:
+        counter2 -= 1
 
-print(counter)
 
+    print(rotate, counter2, number)
+    counter3 = counter2
+    
+print(counter)  
+print(counter2)  
